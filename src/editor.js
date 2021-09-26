303c30cb4a6ee4e4eba57e6e123e8a31b61c37a1
Codablack23 <goodluckedih@yahoo.com>
2021-09-27 00:30:19 +0100

    2 files modified

--- src/editor.js
+++ src/editor.js
@@ -1,31 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function EditorComponent() {
+  const [active, setActive] = useState('html');
+
   return (
     <div className="editor">
       <div className=" d-flex justify-content-between tab-nav">
         <nav>
           <ul>
-            <li className="tab-link mr-3 btn w3-text-grey w3-card-4">HTML</li>
-            <li className="tab-link mr-3 btn w3-text-grey w3-card-4">CSS</li>
-            <li className="tab-link mr-3 btn w3-text-grey w3-card-4">JS</li>
+            <li
+              className="tab-link mr-3 btn w3-text-grey w3-card-4"
+              onClick={this.setActive('html')}
+            >
+              HTML
+            </li>
+            <li
+              className="tab-link mr-3 btn w3-text-grey w3-card-4"
+              onClick={this.setActive('css')}
+            >
+              CSS
+            </li>
+            <li
+              className="tab-link mr-3 btn w3-text-grey w3-card-4"
+              onClick={this.setActive('js')}
+            >
+              JS
+            </li>
           </ul>
         </nav>
         <div className="view">
           <button className="btn w3-red">View</button>
         </div>
       </div>
-      <div className="editor-container">
-        <div className="html">
-          <text-area className="html-text"></text-area>
-        </div>
-        <div className="css">
-          <text-area className="css-text"></text-area>
-        </div>
-        <div className="js">
-          <text-area className="js-text"></text-area>
-        </div>
-      </div>
+      {/* <div className="editor-container">
+        {active == 'html' && (
+          <div className="html">
+            <textarea
+              placeholder="Your HTML Code Goes Here"
+              className="html-text w3-card-4 w3-text-blue"
+            ></textarea>
+          </div>
+        )}
+        {/* {active == 'css' && (
+          <div className="css">
+            <textarea
+              placeholder="Your CSS Code Goes Here"
+              className="css-text w3-card-4 w3-text-green"
+            ></textarea>
+          </div> *
+
+        {active == 'js' && (
+          <div className="js">
+            <textarea
+              placeholder="Your Javascript Code Goes Here"
+              className="js-text w3-card-4 w3-text-red"
+            ></textarea>
+          </div>
+        )}
+      </div> */}
     </div>
   );
 }
