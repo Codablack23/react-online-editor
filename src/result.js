970f5d3d02054893a5cf1b66adf822b42a274cb8
Codablack23 <goodluckedih@yahoo.com>
2021-09-27 01:39:02 +0100

    1 file modified

--- src/result.js
+++ src/result.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Result(result) {
   return (
     <div className="">
+      <h1>me</h1>
       <iframe src="" className="result-frame"></iframe>
     </div>
   );
