a0e8780548a09efb5c55502cce5ffd662568e074
Codablack23 <goodluckedih@yahoo.com>
2021-09-27 02:18:18 +0100

    1 file modified

--- src/result.js
+++ src/result.js
@@ -4,7 +4,12 @@ export default function Result({ result }) {
   return (
     <div className="w3-text-orange">
       <h1>me</h1>
-      <iframe src="https://www.youtube.com/embed/cWDJoK8zw58" Onclick={(e)=>{console.log(e.target)}}></iframe>
+      <iframe
+        src="https://www.youtube.com/embed/cWDJoK8zw58"
+        onClick={(e) => {
+          console.log(e.target);
+        }}
+      ></iframe>
     </div>
   );
 }
