84e63d0095e7ac2e0847901679df4b4c0187f232
Codablack23 <goodluckedih@yahoo.com>
2021-09-27 19:10:24 +0100

    1 file modified

--- src/result.js
+++ src/result.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import Frame from 'react-frame-component';
+import Frame, { FrameContextConsumer } from 'react-frame-component';
 
 export default function Result({ result }) {
   return (
     <div className="w3-text-orange">
       <h1>me</h1>
-      <Frame className="result-frame">{result}</Frame>
+      <Frame>
+        <FrameContextConsumer>
+          {
+            // Callback is invoked with iframe's window and document instances
+            ({ document, window }) => {
+              result;
+            }
+          }
+        </FrameContextConsumer>
+      </Frame>
     </div>
   );
 }
