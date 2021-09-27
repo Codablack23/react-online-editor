0ed4afc4b95e093e5a924f865fd43343951af654
Codablack23 <goodluckedih@yahoo.com>
2021-09-27 01:29:45 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './style.css';
 import './w3css.css';
 import Editor from './editor';
 import Result from './result';
 
 export default function App() {
+  const [code,setCode]=useState("")
+  const changeCode=(end_code)=>{
+    setCode(end_code)
+  }
   return (
     <div className="app">
       <Editor />
